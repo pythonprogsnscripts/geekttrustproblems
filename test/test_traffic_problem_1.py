30142bad803058e5085682bd64dd5f8a43882506
Sunil.Jacob <sunil1405@gmail.com>
2019-08-10 21:34:09 +0530

    Included pytest-1

--- test/test_traffic_problem_1.py
+++ test/test_traffic_problem_1.py
@@ -4,12 +4,16 @@ sys.path.append(os.path.dirname(__file__)+"/../")
 from src import traffic_problem_1 as tp
 import pytest
 
-@pytest.mark.parametrize('climate, speed',          \
-                        [                           \
-                            ('Sunny', -0.1),        \
-                            ('Windy', 0.0),         \
-                            ('Rainy', 0.2)
-                        ])
-def test_when_climate_sunny_return_all_vechicles(climate, speed):
-    crater_speed = tp.getvehicles(climate)
-    assert crater_speed[1] == speed
+# @pytest.mark.parametrize('climate, speed',          \
+#                         [                           \
+#                             ('Sunny', -0.1),        \
+#                             ('Windy', 0.0),         \
+#                             ('Rainy', 0.2)
+#                         ])
+# def test_when_climate_sunny_return_all_vechicles(climate, speed):
+#     crater_speed = tp.getvehicles(climate)
+#     assert crater_speed[1] == speed
+
+def test_when_climate_sunny_return_speed():
+    crater_speed = tp.getvehicles('Sunny')
+    assert crater_speed[1] == -0.1
