48e5503bf2158440eff4c01fdec95b7f120e736b
Sunil.Jacob <sunil1405@gmail.com>
2019-10-26 13:43:54 +0530

    Cleaning class_traffic.py

--- src/class_traffic.py
+++ src/class_traffic.py
@@ -18,14 +18,6 @@ class Vehicle():
         self.cross_cater = cross_cater
         self.can_travel = can_travel
 
-    def weather_suitability(self, wetter):
-        '''
-        This Vehicle class function checks the suitability of the vehicle type(class) with the weather
-        in which it can be driven. It takes a weather object as parameter and checks if the weather
-        object is in the list of permitted weather type in the vehicle class. 
-        '''
-        return wetter in self.can_travel
-
 class Orbit():
     '''
     This Orbit class creates Orbit object/instances with attributes of its route name, distance between
@@ -67,8 +59,3 @@ class Orbit():
                                  ,odata[eachOrbit]['point_two']
                                   )
         return self
-
-# print(os.path.abspath(__file__))
-# print(os.path.dirname(os.path.abspath(__file__)))
-# print(sys.path.append(os.path.dirname(os.path.abspath(__file__))))
-# print(sys.path)
