56d36f607eb62e0d3b4a9e30d9334964261088b3
Sunil.Jacob <sunil1405@gmail.com>
2019-11-08 19:31:35 +0530

    Separated Traffic class

--- src/traffic_problem_1.py
+++ src/traffic_problem_1.py
@@ -4,97 +4,52 @@ King Shan wants to visit the suburb of Hallitharam, and has 2 possible orbits
 and 3 possible vehicles to chose from.The coding challenge is to determine which orbit
 and vehicle King Shan should take to reach Hallitharam the fastest
 '''
+
 import sys
 import os
 import argparse
 sys.path.append(os.path.dirname(__file__)+"/../")
 from src.validate_input_data import validate_input_data
-from src.find_shortest_path import get_shortest_path
+from src.class_traffic import Traffic
 
-class Traffic:
+PARSER = argparse.ArgumentParser(prog='traffic_problem_one',                \
+                            description='Geek Trust traffic problem',       \
+                            allow_abbrev=False)
+
+PARSER.add_argument('Climate', metavar='--climate', action='store',         \
+                    type=str, help='Climate condition')
+PARSER.add_argument('Orbit1', metavar='--orbit1', action='store',           \
+                    type=int, help='Orbit 1 traffic speed')
+PARSER.add_argument('Orbit2', metavar='--orbit2', action='store',
+                    type=int, help='Orbit 2 traffic speed')                 \
+
+def results():
     '''
-    Creating private methods and private attributes
-    Encapsulation
+    This function returns data from the options specified using parse_args()
+    The return value from parse_args() is a Namespace containing the arguments
+    to the command. The object holds the argument values as attributes
     '''
-    def __init__(self, climate='Sunny', traffic_speed_orbit1=12, traffic_speed_orbit2=10):
-        self.__climate = climate
-        self.__traffic_speed_orbit1 = traffic_speed_orbit1
-        self.__traffic_speed_orbit2 = traffic_speed_orbit2
-        self.__shortest_path()
+    return PARSER.parse_args()
 
-    def get_climate(self):
-        '''
-        Getter for climate attribute
-        '''
-        return self.__climate
-
-    def get_set_traffic_speed_orbit1(self):
-        '''
-        Getter for traffic_speed_orbit1 attribute
-        '''
-        return self.__traffic_speed_orbit1
-
-    def get_set_traffic_speed_orbit2(self):
-        '''
-        Getter for traffic_speed_orbit2 attribute
-        '''
-        return self.__traffic_speed_orbit2
-    def set_climate(self, climate):
-        '''
-        Setter for climate attribute
-        '''
-        self.__climate = climate
-
-    def set_traffic_speed_orbit1(self, traffic_speed_orbit_one):
-        '''
-        Setter for traffic_speed_orbit1 attribute
-        '''
-        self.__traffic_speed_orbit1 = traffic_speed_orbit_one
-
-    def set_traffic_speed_orbit2(self, traffic_speed_orbit_two):
-        '''
-        Setter for traffic_speed_orbit2 attribute
-        '''
-        self.__traffic_speed_orbit2 = traffic_speed_orbit_two
-
-    def __shortest_path(self):
-        get_shortest_path(self.__climate, self.__traffic_speed_orbit1, self.__traffic_speed_orbit2)
-
-def create_parser():
+def getVars():
     '''
-        :param arg: Weather condition
-        :param metavar: variable
-        :param action: store the data
-        :param type: data type
-        :param help_message: help_message
+    The functions takes the results and processes. This function
+    also check, whether valid climate condition has been given
+    as input
     '''
-    PARSER = argparse.ArgumentParser(prog='traffic_problem_one',                \
-                                description='Geek Trust traffic problem',       \
-                                allow_abbrev=False)
-
-    PARSER.add_argument('Climate', metavar='--climate', action='store',         \
-                        type=str, help='Climate condition')
-    PARSER.add_argument('Orbit1', metavar='--orbit1', action='store',           \
-                        type=int, help='Orbit 1 traffic speed')
-    PARSER.add_argument('Orbit2', metavar='--orbit2', action='store',
-                        type=int, help='Orbit 2 traffic speed')                 \
-
-    return PARSER
+    res = results()
+    check_climate_input = validate_input_data(res.Climate)
+    if check_climate_input:
+        find_traffic = Traffic()
+        find_traffic.set_climate(res.Climate)
+        find_traffic.set_traffic_speed_orbit1(res.Orbit1)
+        find_traffic.set_traffic_speed_orbit2(res.Orbit2)
 
 def main():
     '''
-    This function will parse the arguments from the user
-    Calls --> validate_input_data(climate)
+    Pedestal function which starts the program
     '''
-    PARSER = create_parser()
-    ARGS = PARSER.parse_args()
-    input_data = [ARGS.Climate, ARGS.Orbit1, ARGS.Orbit2]
-    check_climate_input = validate_input_data(ARGS.Climate)
-    if check_climate_input:
-        find_traffic = Traffic()
-        find_traffic.set_climate(input_data[0])
-        find_traffic.set_traffic_speed_orbit1(input_data[1])
-        find_traffic.set_traffic_speed_orbit2(input_data[2])
+    getVars()
 
 if __name__ == "__main__":
     main()
